6683917712f6a8680be036fed47f725a53133528
Marty Wernimont <mwernimont87@gmail.com>
2020-03-23 14:16:59 -0500

    Quiz setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Quiz />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Quiz from './components/Quiz';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Quiz
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html,body{
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
 }
 </style>
---
+++ src/components/Quiz.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="quiz">
+        <div id="question">Question</div>
+        <div id="options">Options</div>
+    </div>
+</template>
+<script>
+export default {
+    name: "Quiz"
+}
+</script>
+<style scoped lang="scss">
+    #quiz{
+        background: red;
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;;
+        justify-content: center;
+        align-items: center;
+    }
+    #question{
+        background: lime;
+        width: 50vw;
+        height: 30vh;
+    }
+    #options{
+        background: purple;
+        width: 50vw;
+        height: 20vh;
+    }
+</style>
